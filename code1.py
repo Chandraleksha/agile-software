ea307617e89397d94bfb256d2c26e9d7d0d48768
Chandraleksha <115769666+Chandraleksha@users.noreply.github.com>
2023-06-09 12:18:59 +0530

    Create code1.py

---
+++ code1.py
@@ -0,0 +1,12 @@
+if 2==3:
+  print("Two is two")
+else:
+  print("Three is not two")
+print("Outside the condition")
+a = 2
+b = 3
+if a==b:
+  print("a is equal to b")
+else:
+  print("a is not equal to b")
+  print(10/3)
